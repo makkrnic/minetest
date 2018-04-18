0bd73ba507a412036d18bb99b3f27ad76de0c2f4
Mak Krnic <mak@makkrnic.com>
2018-04-18 18:38:18 +0200

    remove unneeded code
    Signed-off-by: Mak Krnic <mak@makkrnic.com>

--- games/alsim/mods/default/mapgen.lua
+++ games/alsim/mods/default/mapgen.lua
@@ -9,88 +9,11 @@ minetest.register_alias("mapgen_dirt_with_grass", "default:dirt_with_grass")
 minetest.register_alias("mapgen_sand", "default:sand")
 minetest.register_alias("mapgen_water_source", "default:water_source")
 minetest.register_alias("mapgen_river_water_source", "default:river_water_source")
-minetest.register_alias("mapgen_lava_source", "default:lava_source")
-minetest.register_alias("mapgen_gravel", "default:gravel")
-
-minetest.register_alias("mapgen_tree", "default:tree")
-minetest.register_alias("mapgen_leaves", "default:leaves")
-minetest.register_alias("mapgen_apple", "default:apple")
-minetest.register_alias("mapgen_junglegrass", "default:junglegrass")
 
 minetest.register_alias("mapgen_cobble", "default:cobble")
-minetest.register_alias("mapgen_stair_cobble", "stairs:stair_cobble")
-minetest.register_alias("mapgen_mossycobble", "default:mossycobble")
 
 
---
--- Ore generation
---
 
-
--- Blob ore first to avoid other ores inside blobs
-
-minetest.register_ore({ 
-	ore_type         = "blob",
-	ore              = "default:clay",
-	wherein          = {"default:sand"},
-	clust_scarcity   = 24*24*24,
-	clust_size       = 7,
-	y_min            = -15,
-	y_max            = 0,
-	noise_threshold = 0,
-	noise_params     = {
-		offset=0.35,
-		scale=0.2,
-		spread={x=5, y=5, z=5},
-		seed=-316,
-		octaves=1,
-		persist=0.5
-	},
-})
-
-minetest.register_ore({
-	ore_type       = "scatter",
-	ore            = "default:stone_with_coal",
-	wherein        = "default:stone",
-	clust_scarcity = 8*8*8,
-	clust_num_ores = 8,
-	clust_size     = 3,
-	y_min          = -31000,
-	y_max          = 64,
-})
-
-minetest.register_ore({
-	ore_type       = "scatter",
-	ore            = "default:stone_with_iron",
-	wherein        = "default:stone",
-	clust_scarcity = 12*12*12,
-	clust_num_ores = 3,
-	clust_size     = 2,
-	y_min          = -15,
-	y_max          = 2,
-})
-
-minetest.register_ore({
-	ore_type       = "scatter",
-	ore            = "default:stone_with_iron",
-	wherein        = "default:stone",
-	clust_scarcity = 9*9*9,
-	clust_num_ores = 5,
-	clust_size     = 3,
-	y_min          = -63,
-	y_max          = -16,
-})
-
-minetest.register_ore({
-	ore_type       = "scatter",
-	ore            = "default:stone_with_iron",
-	wherein        = "default:stone",
-	clust_scarcity = 7*7*7,
-	clust_num_ores = 5,
-	clust_size     = 3,
-	y_min          = -31000,
-	y_max          = -64,
-})
 
 
 --
