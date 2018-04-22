d83795503177c69ae5d303e6aa8647ae93b6a12a
Mak Krnic <mak@makkrnic.com>
2018-04-22 22:49:33 +0200

    add target following
    Signed-off-by: Mak Krnic <mak@makkrnic.com>

--- games/alsim/mods/alsim/init.lua
+++ games/alsim/mods/alsim/init.lua
@@ -160,21 +160,99 @@ minetest.register_abm({
   end
 })
 
+function pick_random_target(pos, max_distance) -- 1/2 Manhattan distance
+  new_pos = pos
+  new_pos.x = pos.x - max_distance + (2 * math.random(1, max_distance))
+  new_pos.z = pos.z - max_distance + (2 * math.random(1, max_distance))
+
+  -- only choose target not higher/lower than max_distance
+  local target_node
+  --while true do
+    local i = 0
+    while true do
+      if i > max_distance then
+        break
+      end
+
+      target_node = minetest.get_node_or_nil(new_pos)
+      if target_node == nil then
+        return new_pos
+      elseif target_node.name ~= "air" then
+        new_pos.y = new_pos.y + 1
+        return new_pos
+      else
+        break
+      end
+
+      i = i + 1
+    end
+  --end
+
+  return new_pos
+end
+
+function atan2(x, y)
+  if x > 0 then
+    return math.atan(y/x)
+  elseif x < 0 and y >= 0 then
+    return math.atan(y/x) + math.pi
+  elseif x < 0 and y < 0 then
+    return math.atan(y/x) - math.pi
+  elseif x == 0 and y > 0 then
+    return math.pi/2
+  elseif x == 0 and y < 0 then
+    return -math.pi/2
+  end
+end
+
 minetest.register_entity("alsim:herbivore", {
   textures = {"alsim_herbivore.png", "alsim_herbivore.png", "alsim_herbivore.png", "alsim_herbivore.png", "alsim_herbivore.png", "alsim_herbivore.png"},
   visual = "cube",
+  collisionbox = {-0.49, -0.49, -0.49, 0.49, 0.49, 0.49, 0.49},
+  physical = true,
+  automatic_rotation = 0.1,
+  automatic_face_movement_dir = 0.0,
+
+  _counted = false,
   on_activate = function(self, staticdata)
-    local sd = minetest.deserialize(staticdata)
-    if sd == nil or not sd.counted then
+    if not self._counted then
       stats_update("herbivores_count", 1)
-      self.counted = true
+      self._counted = true
     end
+
+    self.object:setacceleration({x = 0, y = -10, z = 0})
   end,
   on_death = function()
     stats_update("herbivores_count", -1)
   end,
-  get_staticdata = function()
-    return minetest.serialize({counted = true})
+
+  state = "stand",
+  target = nil,
+  on_step = function(self, dtime)
+    local rn = math.random(1, 1000)
+
+    if self.state == "stand" then
+      if rn <= 30 then
+        self.state = "walk"
+        self.target = pick_random_target(self.object:getpos(), 40)
+      end
+    elseif self.state == "walk" then
+      if rn <= 3 then
+        self.state = "stand"
+      end
+
+      if self.target ~= nil then
+        local sp = self.object:getpos()
+        local vec = {x=self.target.x-sp.x, y=self.target.y-sp.y, z=self.target.z-sp.z}
+        local yaw = atan2(vec.x, vec.z)
+
+        self.object:setyaw(yaw)
+
+        local x = math.sin(yaw) * -5
+        local z = math.cos(yaw) * 5
+        self.object:setvelocity({x = x, y = self.object:getvelocity().y, z = z})
+      end
+    end
   end,
 })
 
